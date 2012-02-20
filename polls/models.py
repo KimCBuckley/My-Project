7c32ca5fc3563ad1fac0740999382ddf35ed1fa7
KimCBuckley <buckley.kimc@gmail.com>
2012-02-20 13:47:36 -0600

    first commit

---
+++ polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __unicode__(self):
+        return self.question
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
+    def __unicode__(self):
+        return self.choice
+
+import datetime
+class Poll(models.Model):
+    def was_published_today(self):
+        return self.pub_date.date() == datetime.date.today()
